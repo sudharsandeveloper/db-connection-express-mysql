7ca3208771cf9c5fae0a71a1acf887c12b4a0208
sudharsandeveloper <sudharsandeveloper@users.noreply.github.com>
2023-12-01 17:47:51 +0530

    register and list using jwt

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'itinafairy',
+  database: 'node_auth_jwt',
 });
 
 connection.connect((err) => {
--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-// app.js
+
 
 const express = require('express');
 const app = express();
-const db = require('./config/db'); // Import the MySQL connection module
+const connection = require('./config/db');
 
-// Your routes and middleware go here
+const jwt = require('jsonwebtoken');
+const secret = require('./config/secret-key.json');
 
-// Example route using the MySQL connection
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
 
-app.get('/users', (req, res) => {
-    db.query('SELECT * FROM users', (error, results) => {
+app.use("/",(req, res)=>{
+    res.json('hello')
+})
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+      return res.status(403).json({ message: 'No token provided' });
+    }
+  
+    jwt.verify(token, secret.secret, (err, decoded) => {
+      if (err) {
+        console.error('Error verifying token:', err);
+        return res.status(401).json({ message: 'Failed to authenticate token' });
+      }
+      next();
+    });
+  };
+
+app.get('/users', verifyToken, (req, res) => {
+    connection.query('SELECT * FROM users', (error, results) => {
       if (error) {
-        console.error('Error executing query:', error);
+        console.error('Error:', error);
         res.status(500).send('Internal Server Error');
         return;
       }
@@ -19,6 +41,21 @@ app.get('/users', (req, res) => {
     });
   });
   
+  app.post('/register', (req, res) => {
+    const {name, email, password, status} = req.body;
+    // console.log(name, email, password, status);
+    // return
+    connection.query('INSERT INTO users (name, email, password, status) VALUES (?, ?, ?, ?)', [name, email, password, status], (err) => {
+        if (err) {
+        console.error('Error registering user:', err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+        }
+
+        const token = jwt.sign({name} , secret.secret);
+
+        res.json({ token });
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
